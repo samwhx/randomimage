031a67c97455d83b9267c84bcec080e6c119f63d
Samuel Wang <samuelwhx@gmail.com>
2018-09-19 15:32:07 +0800

    refactored hash formula into array formula with readdirsync to autoadd file names

--- main.js
+++ main.js
@@ -2,36 +2,29 @@
 const express = require('express')
 const path = require('path')
 
-// load resources
+// multiple static resources
 const resources = ['public']
 
 // start express
 const app = express()
 
+//fs.readdirSync to store all images names inside an array
+const fs = require('fs');
+const imageFolder = path.join(__dirname, 'public', 'images');
+imageFolderContentArray = [];
+fs.readdirSync(imageFolder).forEach(fileName => {
+  imageFolderContentArray.push(fileName)
+})
+
 // random image
 function getImageName () {
-var hash = new Object(); // or just {}
-hash[1] = "deer.jpg";
-hash[2] = "dog.jpg";
-hash[3] = "elephant.jpg";
-hash[4] = "giraffe.jpg";
-hash[5] = "hamster.jpg";
-hash[6] = "kitten.jpg";
-hash[7] = "koala.jpg";
-hash[8] = "panda.jpg";
-hash[9] = "porcupine.jpg";
-hash[10] = "rabbit.jpg";
-hash[11] = "seal.jpg";
-hash[12] = "tortise.jpg";
-hash_hength = Object.keys(hash).length;
-random_number =  Math.floor((Math.random() * hash_hength) + 1);
-random_image_name = hash[random_number];
+random_number =  Math.floor((Math.random() * imageFolderContentArray.length) + 1);
+random_image_name = imageFolderContentArray[random_number];
 return random_image_name
 }
 
 // configure routes
 // static folder for images
-
 app.get('/image', (req, resp) => {
 
   image_name = getImageName()
@@ -43,14 +36,13 @@ app.get('/image', (req, resp) => {
 
 //api
 app.get('/random-image', (req, resp) => {
-
   image_name = getImageName()
-
   resp.status(200)
   resp.type('image/jpg')
   resp.sendfile(path.join(__dirname, 'public', 'images', image_name))
 })
 
+//load multiple static resources
 for (let res of resources) {
   console.info(`loading static resource: ${res}`);
   app.use(express.static(path.join(__dirname, res)));
